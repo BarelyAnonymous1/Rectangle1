6586c1884b6be352d9063d3c0e931328e0ffbac3
Jonathan <Jonathan@DESKTOP-E6U5HEH>
2016-01-27 08:33:13 -0800

    File /C:/Users/Jonathan/workspace/Assignment1/src/Rectangle1.java changed at Wed Jan 27 08:33:12 PST 2016

---
+++ src/Rectangle1.java
@@ -0,0 +1,25 @@
+/**
+ * { your description of the project here }
+ */
+
+/**
+ * The class containing the main method, the entry point of the application.
+ *
+ * @author Jonathan DeFreeuw (jondef95), Preston Lattimer (platt)
+ * @version Rectangle1
+ */
+public class Rectangle1
+{
+
+    /**
+     * The entry point of the application.
+     *
+     * @param args
+     *            The name of the command file passed in as a command line
+     *            argument.
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+    }
+}
