003869d584205ec37deaf12368cd9ec3ddf1b3ed
Jonathan <Jonathan@DESKTOP-E6U5HEH>
2016-01-28 14:07:33 -0500

    File /C:/Users/Jonathan/workspace/Assignment1/src/Rectangle1Test.java changed at Thu Jan 28 14:07:33 EST 2016

--- src/Rectangle1Test.java
+++ src/Rectangle1Test.java
@@ -7,24 +7,24 @@ import student.TestCase;
 public class Rectangle1Test extends TestCase
 {
 
-        /**
-         * This method sets up the tests that follow.
-         */
-        public void setUp()
-        {
-                // Nothing here yet
-        }
+    /**
+     * This method sets up the tests that follow.
+     */
+    public void setUp()
+    {
+        // Nothing here yet
+    }
 
-        // ----------------------------------------------------------
-        /**
-         * This method is simply to get code coverage of the class declaration.
-         */
-        public void testRInit()
-        {
-                Rectangle1 dum = new Rectangle1();
-                assertNotNull(dum);
-                Rectangle1.main(null);
-                assertFuzzyEquals("Hello, World\n", systemOut().getHistory());
-        }
+    // ----------------------------------------------------------
+    /**
+     * This method is simply to get code coverage of the class declaration.
+     */
+    public void testRInit()
+    {
+        Rectangle1 dum = new Rectangle1();
+        assertNotNull(dum);
+        Rectangle1.main(null);
+        assertFuzzyEquals("Hello, World\n", systemOut().getHistory());
+    }
 
 }
