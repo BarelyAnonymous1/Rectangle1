9ccff7aa0381d402d64547327dbf64a1962bf33e
Jonathan <Jonathan@DESKTOP-E6U5HEH>
2016-01-28 15:13:25 -0500

    File /C:/Users/Jonathan/workspace/Assignment1/src/CommandParser.java changed at Thu Jan 28 15:13:25 EST 2016

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -2,29 +2,48 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * CommandParser class used to scan through a file with a Scanner and 
+ * retrieve specific values to create a SkipList of Rectangles
+ * 
+ * @author Jonathan DeFreeuw (jondef95), Preston Lattimer (platt)
+ *
+ */
 public class CommandParser
 {
-    private Scanner scanner;
+    /**
+     * String field to hold the file that has been input
+     */
+    private String inputFile;
 
+    /**
+     * constructor for parser, stores filename
+     * @param file  name for the file that is being parsed
+     */
     public CommandParser(String file)
     {
+        inputFile = file;
     }
 
     public void parse()
     {
+        Scanner scanner = null;
         try
         {
-            scanner = new Scanner(new File(file));
+            scanner = new Scanner(new File(inputFile));
         }
         catch (FileNotFoundException e)
         {
             e.printStackTrace();
             System.out.println(e.getMessage());
         } // Create new scanner
-        while (scanner.hasNext())
-        { // While the scanner has information to read
-            String cmd = scanner.next(); // Read the next term
-            System.out.println("cmd");
+        if (scanner != null)
+        {
+            while (scanner.hasNext())
+            { // While the scanner has information to read
+                String cmd = scanner.next(); // Read the next term
+                System.out.println(cmd);
+            }
         }
     }
 }
