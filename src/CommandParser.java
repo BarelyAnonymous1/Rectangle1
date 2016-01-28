2e9c547915395d7e72cc13aa420e2a48e7ab17ea
Jonathan <Jonathan@DESKTOP-E6U5HEH>
2016-01-28 11:46:50 -0500

    File /C:/Users/Jonathan/workspace/Assignment1/src/Rectangle1Test.java changed at Thu Jan 28 11:46:50 EST 2016

--- src/CommandParser.java
+++ src/CommandParser.java
@@ -4,48 +4,24 @@ import java.util.Scanner;
 
 public class CommandParser
 {
-	private Scanner scanner;
-	public CommandParser(String file)
-	{
-        try
+        private Scanner scanner;
+
+        public CommandParser(String file)
         {
-			scanner = new Scanner(new File(file));
-		} 
-        catch (FileNotFoundException e)
+                try
+                {
+                        scanner = new Scanner(new File(file));
+                } catch (FileNotFoundException e)
+                {
+                        e.printStackTrace();
+                } // Create our new scanner
+        }
+
+        public void parse()
         {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}//Create our new scanner
-	}
-	public void parse(){
-	         while(scanner.hasNext()){//While the scanner has information to read
-	            String cmd = scanner.next();//Read the next term
-	            double x; double y; double radius;
-	            switch(cmd){
-	               case "debug" :
-	                  System.out.println("debug cmd activated");
-	               break;
-	               case "add" ://Found an add command
-	                  x = scanner.nextDouble();
-	                  y = scanner.nextDouble();
-	                  String name = scanner.next();
-	                  System.out.println("Insert node at "+x+" "+y+" with name "+name);
-	               break;
-	               case "delete" ://Found a delete command
-	                  x = scanner.nextDouble();
-	                  y = scanner.nextDouble();
-	                  System.out.println("Remove node at "+x+" "+y);
-	               break;
-	               case "search" ://Found a search command
-	                  x = scanner.nextDouble();
-	                  y = scanner.nextDouble();
-	                  radius = scanner.nextDouble();
-	                  System.out.println("Search for node near "+x+" "+y+" within radius of "+radius);
-	               break;
-	               default ://Found an unrecognized command
-	                  System.out.println("Unrecognized input "+cmd);
-	               break;
-	            }
-	         }
-	      }
+                while (scanner.hasNext())
+                {// While the scanner has information to read
+                        String cmd = scanner.next();// Read the next term
+                }
+        }
 }
