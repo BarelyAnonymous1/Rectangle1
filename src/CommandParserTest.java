19e3b830fdf7385e6e5047cca7eaf4aadcc03f40
Jonathan <Jonathan@192.168.1.7>
2016-01-28 21:08:22 -0500

    File /C:/Users/Jonathan/git/Rectangle1/src/CommandParserTest.java changed at Thu Jan 28 21:08:22 EST 2016

--- src/CommandParserTest.java
+++ src/CommandParserTest.java
@@ -15,6 +15,9 @@ public class CommandParserTest extends TestCase
 {
     private CommandParser parser;
 
+    /**
+     * tests to see if the parser will correctly handle an incorrect file
+     */
     public void testSearchFail()
     {
         parser = new CommandParser("fail");
@@ -22,6 +25,9 @@ public class CommandParserTest extends TestCase
         assertFalse(success);
     }
     
+    /**
+     * tests to see if the parser will correctly open and close a file
+     */
     public void testSearchPass()
     {
         parser = new CommandParser("SyntaxTest.txt");
