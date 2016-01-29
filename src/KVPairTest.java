1c7124b45d6138c4b0add7b31f50e36cad20b738
Jonathan <Jonathan@DESKTOP-E6U5HEH>
2016-01-29 13:27:23 -0500

    File /C:/Users/Jonathan/workspace/Rectangle1/src/KVPairTest.java changed at Fri Jan 29 13:27:23 EST 2016

---
+++ src/KVPairTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import student.TestCase;
+public class KVPairTest
+{
+
+    @Test
+    public void testKVPair()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCompareToKVPairOfKE()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCompareToK()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testKey()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testValue()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString()
+    {
+        fail("Not yet implemented");
+    }
+
+}
