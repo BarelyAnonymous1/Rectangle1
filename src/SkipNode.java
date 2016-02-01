bc4c3f8f1e7f950f15f45d6d0cb26f292be45f46
Preston Lattimer <preston.lattimer@gmail.com>
2016-02-01 14:19:15 -0500

    File /Users/prestonlattimer/git/Rectangle1/src/SkipNode.java changed at Mon Feb 01 14:19:15 EST 2016

--- src/SkipNode.java
+++ src/SkipNode.java
@@ -13,7 +13,12 @@ public class SkipNode<K extends Comparable<K>, E> {
 	 * creates a skip list node that is blank that 
 	 * will be the one we actually are creating
 	 */
-	private SkipNode<K,E> up, down, next, previous;
+	private SkipNode<K,E> 
+	up, 
+	down, 
+	next, 
+	previous;
+	
 	/**
 	 * creates a private key for this node;
 	 */
@@ -52,20 +57,66 @@ public class SkipNode<K extends Comparable<K>, E> {
 	}
 	
 	/**
-	 * @param node is the previuos node
+	 * --------------------------------
+	 * @param node is the previous node
 	 */
 	public void setPrevious(SkipNode node)
 	{
 		previous = node;
 	}
+	/**
+	 * @return previous node 
+	 */
+	public SkipNode getPrevious()
+	{
+		return previous;
+	}
 	
 	/**
+	 * --------------------------------
 	 * @param node is the next node
 	 */
 	public void setNext(SkipNode node)
 	{
 		next = node;
 	}
+	/**
+	 * @return next node 
+	 */
+	public SkipNode getNext()
+	{
+		return next;
+	}
 	
+	/**
+	 * --------------------------------
+	 * @param node is the node above
+	 */
+	public void setUp(SkipNode node)
+	{
+		up = node;
+	}
+	/**
+	 * @return up node 
+	 */
+	public SkipNode getUp()
+	{
+		return up;
+	}
 	
+	/**
+	 * --------------------------------
+	 * @param node is the node below
+	 */
+	public void setDown(SkipNode node)
+	{
+		down = node;
+	}
+	/**
+	 * @return down node 
+	 */
+	public SkipNode getDown()
+	{
+		return down;
+	}
 }
