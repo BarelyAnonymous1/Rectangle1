633ef54b5b844bb622ccba81629dc5e8984089cb
jondef95@vt.edu <jondef95@github.com>
2016-01-31 20:34:10 -0500

    File /C:/Users/Jonathan/git/Rectangle1/src/Rectangle.java changed at Sun Jan 31 20:34:09 EST 2016

---
+++ src/Rectangle.java
@@ -0,0 +1,81 @@
+
+@SuppressWarnings("rawtypes")
+public class Rectangle implements Comparable
+{
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public Rectangle()
+    {
+        x = 0;
+        y = 0;
+        width = 0;
+        height = 0;
+    }
+
+    public Rectangle(int newX, int newY, int newWidth, int newHeight)
+    {
+        x = newX;
+        y = newY;
+        width = newWidth;
+        height = newHeight;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public void setX(int newX)
+    {
+        x = newX;
+    }
+
+    public void setY(int newY)
+    {
+        y = newY;
+    }
+
+    public void setWidth(int newWidth)
+    {
+        width = newWidth;
+    }
+
+    public void setHeigh(int newHeight)
+    {
+        height = newHeight;
+    }
+
+    @Override
+    public int compareTo(Object otherRect) throws ClassCastException
+    {
+        // TODO Auto-generated method stub
+        if (!(otherRect instanceof Rectangle))
+        {
+            throw new ClassCastException("A Recangle object was expected");
+        }
+        else
+        {
+            return 
+        }
+        return 0;
+    }
+
+}
