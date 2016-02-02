ded7129dd574db64dde3f471bd3b20573a4cb662
Preston Lattimer <preston.lattimer@gmail.com>
2016-02-01 22:15:12 -0500

    File /Users/prestonlattimer/git/Rectangle1/src/SkipListTest.java changed at Mon Feb 01 22:15:12 EST 2016

--- src/SkipListTest.java
+++ src/SkipListTest.java
@@ -1,17 +1,35 @@
-import static org.junit.Assert.*;
+import student.TestCase;
 
-import org.junit.Before;
-import org.junit.Test;
+public class SkipListTest extends TestCase {
+	
+	private SkipList<String, Integer> list;
+	private SkipNode<String, Integer> node1;
+	private SkipNode<String, Integer> node2;
+	private SkipNode<String, Integer> node3;
+	private KVPair<String, Integer> pair1;
+	private KVPair<String, Integer> pair2;
+	private KVPair<String, Integer> pair3;
+	
 
-public class SkipListTest {
-
-	@Before
-	public void setUp() throws Exception {
+	/**
+	 * sets up the test cases
+	 */
+	public void setUp()
+	{
+		pair1 = new KVPair<String, Integer>("node1", 1);
+		pair2 = new KVPair<String, Integer>("node2", 2);
+		pair3 = new KVPair<String, Integer>("node3", 3);
+		list = new SkipList<String, Integer>();
 	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	/**
+	 * tests the insert method
+	 */
+	public void testInsert()
+	{
+		list.insert(pair1);
+		list.dump();
 	}
-
+	
+	
 }
