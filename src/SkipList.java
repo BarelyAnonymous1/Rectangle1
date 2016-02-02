d5428b5521b33def7a166799ea80922cbfb0c3e9
jondef95@vt.edu <jondef95@github.com>
2016-02-01 20:44:41 -0500

    File /C:/Users/Jonathan/git/Rectangle1/src/SkipList.java changed at Mon Feb 01 20:44:41 EST 2016

--- src/SkipList.java
+++ src/SkipList.java
@@ -4,10 +4,29 @@
  * @author Jonathan
  *
  */
-public class SkipList
+public class SkipList<K extends Comparable<K>, E>
 {
     /**
      * head node
      */
-    private SkipNode head;
+    private SkipNode<K, E> head;
+    
+    public SkipList()
+    {
+        head = new SkipNode<K, E>(0);
+    }
+    
+    public void insert(KVPair<K, E> newPair)
+    {
+        SkipNode<K, E> newNode = new SkipNode<K, E>(newPair, 0);
+        if (head.getNext() == null)
+        {
+            head.setNext(newNode);
+        }
+        else
+        {
+            newNode.setNext(head.getNext());
+            head.setNext(newNode);
+        }
+    }
 }
