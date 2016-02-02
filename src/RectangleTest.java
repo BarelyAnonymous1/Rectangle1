8668847361c25c8e4f2b99e287c05cfb96443f3d
jondef95@vt.edu <jondef95@github.com>
2016-02-01 19:58:23 -0500

    File /C:/Users/Jonathan/git/Rectangle1/src/RectangleTest.java changed at Mon Feb 01 19:58:22 EST 2016

---
+++ src/RectangleTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RectangleTest
+{
+
+    public void testCompareTo()
+    {
+        Rectangle rect1 = new Rectangle(0, 0, 10, 10);
+        Rectangle rect2 = new Rectangle(0, 0, 10, 10);
+        Rectangle rect3 = new Rectangle(10, 11, 13, 13);
+
+        assertTrue(rect1.compareTo(rect2) == 0);
+
+    }
+
+}
