f9334a6da2dda37814ad97bceb06404e9fd68b53
Preston Lattimer <preston.lattimer@gmail.com>
2016-02-01 21:20:16 -0500

    File /Users/prestonlattimer/git/Rectangle1/src/SkipNodeTest.java changed at Mon Feb 01 21:20:15 EST 2016

---
+++ src/SkipNodeTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+/**
+ * @author prestonlattimer
+ *
+ */
+public class SkipNodeTest {
+
+	
+	private SkipNode<String, Integer> node1;
+	private SkipNode<String, Integer> node2;
+	private SkipNode<String, Integer> node3;
+	private KVPair<String, Integer> pair;
+	/**
+	 * sets up the test cases
+	 */
+	public void setUp()
+	{
+		pair = new KVPair<String, Integer>("first", 1);
+		node1 = new SkipNode<String, Integer>(1);
+		node2 = new SkipNode<String, Integer>(pair, 1);
+	}
+
+}
