b26f5d9c07ec725c43465ba513a1be245e56d488
jondef95@vt.edu <jondef95@github.com>
2016-02-01 20:20:18 -0500

    File /C:/Users/Jonathan/git/Rectangle1/src/RectangleTest.java changed at Mon Feb 01 20:20:17 EST 2016

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -117,4 +117,15 @@ public class RectangleTest extends TestCase
 
         assertTrue(rect1.compareTo(rect2) == -1);
     }
+    
+    /**
+     * tests when the compareTo should be false
+     */
+    public void testCompareToFalseH()
+    {
+        Rectangle rect1 = new Rectangle(0, 0, 10, 10);
+        Rectangle rect2 = new Rectangle(0, 0, 10, 11);
+
+        assertTrue(rect1.compareTo(rect2) == -1);
+    }
 }
