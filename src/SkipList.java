3502c359a1de3428d315a1245ec0022ca4b9950f
jondef95@vt.edu <jondef95@github.com>
2016-02-01 20:37:55 -0500

    File /C:/Users/Jonathan/git/Rectangle1/src/SkipList.java changed at Mon Feb 01 20:37:54 EST 2016

---
+++ src/SkipList.java
@@ -0,0 +1,13 @@
+
+/**
+ * SkipList that will be used to hold rectnagles input from a file
+ * @author Jonathan
+ *
+ */
+public class SkipList
+{
+    /**
+     * head node
+     */
+    private SkipNode head;
+}
