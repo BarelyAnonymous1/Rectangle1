8538a34537a64655c7270146ba3844916c57f62b
Jonathan <jondef95@vt.edu>
2016-02-06 16:10:00 -0500

    File /C:/Users/Jonathan/workspace/Rectangle1/src/SkipList.java changed at Sat Feb 06 16:10:00 EST 2016

--- src/SkipList.java
+++ src/SkipList.java
@@ -177,7 +177,7 @@ public class SkipList<K extends Comparable<K>, E>
         {
             while (current.next[i] != null)
             {
-                System.out.println(current.getValue());
+                System.out.println(current.next[i].getValue());
                 if (current.next[i].getValue().equals(value))
                 {
                     if (locatedKey == null)
