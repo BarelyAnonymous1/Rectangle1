cad199f3b15aba1f8702ffddf018228a47a546a1
Jonathan <jondef95@vt.edu>
2016-02-08 20:06:02 -0500

    File /C:/Users/Jonathan/workspace/Rectangle1/src/SkipNodeTest.java changed at Mon Feb 08 20:06:01 EST 2016

--- src/SkipNodeTest.java
+++ src/SkipNodeTest.java
@@ -7,8 +7,8 @@ import student.TestCase;
 public class SkipNodeTest extends TestCase
 {
 
-    private SkipNode<String, Integer> node2;
-    private SkipNode<String, Integer> node3;
+    private SkipNode<String, Integer> nodeNull;
+    private SkipNode<String, Integer> nodeTrue;
     private KVPair<String, Integer>   pair;
 
     /**
@@ -17,7 +17,7 @@ public class SkipNodeTest extends TestCase
     public void setUp()
     {
         pair = null;
-        node2 = new SkipNode<String, Integer>(pair, 1);
+        nodeNull = new SkipNode<String, Integer>(pair, 1);
     }
 
     /**
@@ -25,7 +25,7 @@ public class SkipNodeTest extends TestCase
      */
     public void testGetKey()
     {
-        assertNull(node2.getKey());
+        assertNull(nodeNull.getKey());
     }
 
     /**
@@ -33,18 +33,20 @@ public class SkipNodeTest extends TestCase
      */
     public void testGetPair()
     {
-        assertNull(node2.getPair());
+        assertNull(nodeNull.getPair());
     }
 
     /**
-     * tests that a node with a KVPair returns that pair by testing its key
+     * tests that a node with a KVPair returns that pair by testing its key,
+     * also tests whether the node returns the correct key
      */
     public void testGetPairRight()
     {
         KVPair<String, Integer> newPair = new KVPair<String, Integer>(
                 "hello!", 1);
-        node3 = new SkipNode<String, Integer>(newPair, 4);
-        assertFuzzyEquals(node3.getPair().key(), "hello!");
+        nodeTrue = new SkipNode<String, Integer>(newPair, 4);
+        assertFuzzyEquals(nodeTrue.getPair().key(), "hello!");
+        assertFuzzyEquals(nodeTrue.getKey(), "hello!");
     }
 
 }
